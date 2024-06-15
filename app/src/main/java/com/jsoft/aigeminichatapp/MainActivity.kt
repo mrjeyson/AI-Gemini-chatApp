13d4b6c0679988c83bfc8caa5eb03dec93fdec3d
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2024-06-15 10:13:12 +0500

    ...

--- app/src/main/java/com/jsoft/aigeminichatapp/MainActivity.kt
+++ app/src/main/java/com/jsoft/aigeminichatapp/MainActivity.kt
@@ -67,6 +67,7 @@ class MainActivity : ComponentActivity() {
             uriState.update { uri.toString() }
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {

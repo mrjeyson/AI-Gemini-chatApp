4b050a6ed6663a11c588db35b9ad229c0c910314
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2025-03-20 14:39:49 +0900

    ...

--- app/src/main/java/com/jsoft/aigeminichatapp/MainActivity.kt
+++ app/src/main/java/com/jsoft/aigeminichatapp/MainActivity.kt
@@ -79,7 +79,6 @@ class MainActivity : ComponentActivity() {
 
 
 
-
     private val uriState = MutableStateFlow("")
     private val imagePicker = registerForActivityResult(
         ActivityResultContracts.PickVisualMedia()
